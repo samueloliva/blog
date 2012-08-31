17841e9fe0b8202e2f8ea44b71ec1875acdf1ac2
Samuel Zanferdini Oliva <szanfer@hotmail.com>
2012-08-31 17:28:23 -0300

    Gmail SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,14 @@ Blog::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'blog-samuel.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'blog-samuel.herokuapp.com',
+    :user_name            => 'blogsamuel123@gmail.com',
+    :password             => 'sp201287',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
